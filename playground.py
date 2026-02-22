40f4b29d74a3008e5827cb138d55bd2c2afdf187
Noha Shehab <nohashehab.iti@gmail.com>
2026-02-22 12:27:19 +0200

    functions

--- playground.py
+++ playground.py
@@ -5,3 +5,40 @@ data =( "noha")
 # I need to define it as a tuple
 
 data = ("noha",)
+
+"""
+
+    int num = 10;
+    if(num==10){}
+
+"""
+
+num = 10
+if num == 10:
+    pass  # pass is a null operation —
+    # when it is executed,
+    # nothing happens. It is
+
+print(4,34,5)
+
+
+temp = 'We are {} students in {} course in track {}'
+print(temp.format(16, 'python', 'gis'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
